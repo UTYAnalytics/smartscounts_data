6728d8b15030fa7e7460987258c0ac0402dad542
UTYAnalytics <54933836+UTYAnalytics@users.noreply.github.com>
2024-03-09 21:07:47 +0700

    Update bundle_smartscounts.py

--- bundle_smartscounts.py
+++ bundle_smartscounts.py
@@ -42,10 +42,10 @@ email_address = "uty.tra@thebargainvillage.com"
 email_password = "kwuh xdki tstu vyct"
 subject_filter = "Keepa.com Account Security Alert and One-Time Login Code"
 
-display = Display(visible=0, size=(800, 800))
-display.start()
+# display = Display(visible=0, size=(800, 800))
+# display.start()
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+# chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
 
 # Create a temporary directory for downloads
 with tempfile.TemporaryDirectory() as download_dir:
